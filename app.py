cdd6df607b3ebeced040e33b9902baeb0126a422
cthuleen <144190065+cthuleen@users.noreply.github.com>
2024-04-09 23:19:50 -0400

    Updated N2YO section of app.py to include info on sat id and name

--- app.py
+++ app.py
@@ -45,13 +45,13 @@ def process():
    response = requests.get(f'https://api.n2yo.com/rest/v1/satellite/above/{lat}/{long}/0/10/0/&apiKey=') #key hidden (Dont Forget to Readd)
    data = response.json()  #returns a dictionary with two keys: info (metadata) and above (the actual satellite info)
    sat_info = data["above"]    #sat_info is the array containing a dictionary for each satellite
-   years = []
+   satellites = []
    for satellite in sat_info:
-      years.append(satellite["intDesignator"][:4])
-   if len(years) == 0:
+      satellites.append([satellite['satid'], satellite['satname'], satellite["intDesignator"][:4]])
+   if len(satellites) == 0:
       print("Sorry, no satellites ahead.")        #no satellites found case
-   random_index = random.randint(0, len(years))    #get a random index to choose a random year
-   print(years[random_index])
+   random_index = random.randint(0, len(satellites))    #get a random index to choose a random year
+   print(satellites[random_index])
 
    #--------------------------------------------------------------------------------------------------------------
    db = sqlite3.connect('DataBase.db')
