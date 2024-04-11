83ed89a071cb198c67593bafeadc0368839bd39b
Sun B <98289547+SunehB@users.noreply.github.com>
2024-04-11 13:44:57 -0400

    Update app.py to include spotify by year

--- app.py
+++ app.py
@@ -77,22 +77,40 @@ def process():
    insertRow(db, cursor, id, date, song, client, satid)
     #--------------------------------------------------------------------------------------------------------------
     access_token = ''
-   query = 'Yesterday artist:The Beatles'
-   year = 1969
-   url = f'https://api.spotify.com/v1/search?q={requests.utils.quote(query)}&type=track'
-
-   headers = {
-      'Authorization': f'Bearer {access_token}',
-      'Content-Type': 'application/json'
-   }
-
-   response = requests.get(url, headers=headers)
-
-   try:
-      data = response.json()
-      print(data)
-   except Exception as err:
-      print(err)
+    year = 1969
+    search_query = f'year:{year}'
+      
+    url = 'https://api.spotify.com/v1/search'
+      
+    headers = {
+          'Authorization': f'Bearer {access_token}',
+          'Content-Type': 'application/json'
+    }
+    params = {
+          'q': search_query,
+          'type': 'track',
+          'limit': 50,  
+          'market': 'US' 
+    }
+      
+      
+    response = requests.get(url, headers=headers, params= params)
+      
+      
+      
+    try:
+          data = response.json()
+          tracks = data['tracks']['items']
+          for track in tracks[:10]: 
+              print(f"Track Name: {track['name']}")
+              print(f"Popularity: {track['popularity']}")
+              print(f"Album Name: {track['album']['name']}")
+              print(f"Artists: {', '.join(artist['name'] for artist in track['artists'])}")
+              print(f"Release Date: {track['album']['release_date']}")
+              print(f"Spotify URL: {track['external_urls']['spotify']}\n")
+          
+    except Exception as err:
+          print(err)
   
    return render_template('index.html')
 
