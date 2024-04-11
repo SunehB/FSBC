874da0f70082d70e5c7107ad58eeddee423f6090
SundB1 <sunehb@bu.edu>
2024-04-11 12:39:31 -0400

    spotifysearch converted to python

--- myproject/spotifysearch.js
+++ myproject/spotifysearch.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
-const accessToken = ''; 
+const accessToken = 'BQCnkpOJOEXQKqVyy-uR86Gyv-_QX1gQJ9PrjGt3dTHEV8Vy7FB0tL839qct58Jt7p9fcn_twYc9nb4rHwgE6IFd2NQ7Xj3GNt8SMKLfj5fkOxs7KZE'; 
 const query = encodeURIComponent('Yesterday artist:The Beatles');
 const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
 
--- myproject/spotifysearch.py
+++ myproject/spotifysearch.py
@@ -1,1 +1,18 @@
+import requests
 
+access_token = 'BQB2UWHHvPaFYJIfvjeB2Hgp-dfuXHVYn0NrTT5uPyyF24v2StqkHQyUp0yAALaIG2lwqEtCu9hbyIsIQLTMxXNQOavhzwKLt1A9x_W_z0i73XgMo4k'
+query = 'Yesterday artist:The Beatles'
+url = f'https://api.spotify.com/v1/search?q={requests.utils.quote(query)}&type=track'
+
+headers = {
+    'Authorization': f'Bearer {access_token}',
+    'Content-Type': 'application/json'
+}
+
+response = requests.get(url, headers=headers)
+
+try:
+    data = response.json()
+    print(data)
+except Exception as err:
+    print(err)
