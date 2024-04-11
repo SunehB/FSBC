2d7bf79725af341ec23a53624a9cb962273c7295
Sun B <98289547+SunehB@users.noreply.github.com>
2024-04-11 13:26:42 -0400

    Update app.py to inbclude spotify search

--- app.py
+++ app.py
@@ -76,7 +76,24 @@ def process():
    satid = years[random_index]
    insertRow(db, cursor, id, date, song, client, satid)
     #--------------------------------------------------------------------------------------------------------------
+    access_token = ''
+   query = 'Yesterday artist:The Beatles'
+   year = 1969
+   url = f'https://api.spotify.com/v1/search?q={requests.utils.quote(query)}&type=track'
 
+   headers = {
+      'Authorization': f'Bearer {access_token}',
+      'Content-Type': 'application/json'
+   }
+
+   response = requests.get(url, headers=headers)
+
+   try:
+      data = response.json()
+      print(data)
+   except Exception as err:
+      print(err)
+  
    return render_template('index.html')
 
 if __name__ == '__main__':
