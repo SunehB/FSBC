a88e76d294925c3125cb41ae8a24a42fc500648e
Fernando Matos <platano2028@gmail.com>
2024-04-11 02:45:21 -0400

    changed the loop so that its now a method called get_sat_data() which just returns the data of the json file. commented the rest which can be used later

--- N2YO_call.py
+++ N2YO_call.py
@@ -7,20 +7,23 @@ import random
 lat = 42.360
 long = -71.059
 
-response = requests.get(f'https://api.n2yo.com/rest/v1/satellite/above/{lat}/{long}/0/10/0/&apiKey=') #key hidden
+def get_sat_data():
+    response = requests.get(f'https://api.n2yo.com/rest/v1/satellite/above/{lat}/{long}/0/10/0/&apiKey=') #key hidden
 
-data = response.json()  #returns a dictionary with two keys: info (metadata) and above (the actual satellite info)
+    data = response.json()  #returns a dictionary with two keys: info (metadata) and above (the actual satellite info)
 
-sat_info = data["above"]    #sat_info is the array containing a dictionary for each satellite
+    # sat_info = data["above"]    #sat_info is the array containing a dictionary for each satellite
 
-satellites = []
+    # satellites = []
 
-for satellite in sat_info:
-    satellites.append([satellite['satid'], satellite['satname'], satellite["intDesignator"][:4]])
+    # for satellite in sat_info:
+    #     satellites.append([satellite['satid'], satellite['satname'], satellite["intDesignator"][:4]])
 
-if len(satellites) == 0:
-    print("Sorry, no satellites ahead.")        #no satellites found case
+    # if len(satellites) == 0:
+    #     print("Sorry, no satellites ahead.")        #no satellites found case
 
-random_index = random.randint(0, len(satellites))    #get a random index to choose a random year
+    # random_index = random.randint(0, len(satellites))    #get a random index to choose a random year
 
-print(satellites[random_index])
+    # print(satellites[random_index])
+
+    return data
